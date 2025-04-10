a1c3ea31dbb69621dc721469eb3304b355df2ff2
Luisa <grabowskiluisa@gmx.de>
2025-04-10 21:34:27 +0200

    legra-technik improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import {
+  RouterLink,
+  RouterOutlet,
+  RouterLinkActive,
+  Router,
+} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, RouterLinkActive],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'legra-technik';
+  constructor(private router: Router) {}
+
+  isHomeRouteActive(): boolean {
+    return this.router.url === '/'; // exakt
+  }
+
+  isAboutUsRouteActive(): boolean {
+    return this.router.url === '/about-us';
+  }
+
+  isContactRouteActive(): boolean {
+    return this.router.url === '/contact';
+  }
 }
